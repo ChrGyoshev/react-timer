60f4e07c49a31457ee56c6bfaefae39b1b353038
ChrGyoshev <krischan@abv.bg>
2023-12-24 16:57:52 +0200

    added timer component and some styling to it

--- timer/src/components/Timer.jsx
+++ timer/src/components/Timer.jsx
@@ -1,7 +1,25 @@
+import styles from "./Timer.module.css";
+import { useState, useEffect } from "react";
 function Timer() {
+  const [started, setIsStarted] = useState(false);
   return (
     <>
-      <h1>Hello from Timer</h1>
+      <section className={styles["clock-wrapper"]}>
+        <article>
+          <h1 className={styles.date}>Timer</h1>
+        </article>
+
+        <article>
+          <h1 className={styles.clock}>00:00:00</h1>
+        </article>
+        <div className={styles["button-wrapper"]}>
+          <button type="button" onClick={() => setIsStarted(!started)}>
+            {started ? "Pause" : "Start"}
+          </button>
+          <button type="button">Stop</button>
+          <button type="button">Reset</button>
+        </div>
+      </section>
     </>
   );
 }
